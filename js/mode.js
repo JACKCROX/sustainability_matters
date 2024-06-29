b483c63f2c90496b66aae4be875089ed98e41099
LINHTET <144081634+JACKCROX@users.noreply.github.com>
2024-06-30 03:06:13 +0800

    Delete js directory

--- js/mode.js
+++
@@ -1,6 +0,0 @@
-function myFunction(){
-    var element = document.body;
-    element.dataset.bsTheme =
-    element.dataset.bsTheme == "light" ? "dark" : "light"; //toggle between light and dark mode (if light, change to dark, if dark, change to light)
-}
-
